efbc203d4349a3e285ed2979e448e256f7ab3f32
Lucas Araujo <contatodevaraujo@gmail.com>
2026-06-02 21:11:25 -0300

    fix(commands): add explicit any type to catch block in deletePage command

--- src/commands/delete-page.ts
+++ src/commands/delete-page.ts
@@ -13,14 +13,14 @@ export const deletePage = new Command("delete-page")
 async function main(names: Array<string>, options: { private: boolean }) : Promise<void> {
   const pageService = new PageService();
 
-  logger.info(`enumerating pages: ${names.length}, group: ${options.private ? 'private' : 'public'}`);
+  logger.info(`enumerating pages: ${names.length}, group: ${options.private ? 'private' : 'public'}`, false);
   
   try {
     await pageService.deletePage(names, options.private);
     logger.info(`resolving 100% ${names.length}/${names.length}, done`, false);
     logger.success(`session: ${logger.getSessionID()}, audit: ${logger.formatLink(logger.getAuditPath())}`, false);
-  } catch (err) {
-    logger.error("unexpected error ocurred:", err);
+  } catch (err: any) {
+    logger.error("unexpected error ocurred", err);
     process.exit(1);
   }
 }
