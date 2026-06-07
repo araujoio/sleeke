f01a8a0695d6e34978e028a44f2f260c712e0700
Lucas Araujo <contatodevaraujo@gmail.com>
2026-06-06 21:10:57 -0300

    refactor(commands): inline initialization logic and remove init service

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,25 +1,34 @@
 import { Command } from "commander";
-import { InitService } from "@/services/init-service";
-import { logger } from "@/services/logger-service";
+import { logger } from "@/utils/logger-service";
+import { fileURLToPath } from "url";
 import path from "path";
+import fsExtra from "fs-extra/esm";
 
 export const init = new Command("init")
   .name("init")
-  .alias("create")
   .description("initialize a new nextjs project")
   .argument("<name>", "the name for the new project.")
   .action(async (name: string) => main(name)
 );
 
-async function main(name: string) : Promise<void> {
-  const initService = new InitService();
-  
+async function main(name: string): Promise<void> {
   try {
-    await initService.initializeProject(name);
-    logger.info(`initialized empty next-app project in: ${path.join(process.cwd(), name)}`, false);
-  } catch (err) {
-    logger.error("initialization error:", err, false);
+    const rootPath: string = path.join(process.cwd(), name);
+    const templatePath: string = path.join(path.dirname(fileURLToPath(import.meta.url)), "templates", "next-app");
+
+    // check if the folder already exists
+    if (fsExtra.pathExistsSync(rootPath)) {
+      throw new Error(`Cannot create Kitraw project. The folder "${name}" already exists at "${rootPath}".`);
+    }
+
+    // check if the kitraw app already exists
+    if (!fsExtra.pathExistsSync(path.join(process.cwd(), "kitraw.json"))) {
+      fsExtra.copySync(templatePath, rootPath);
+      logger.info(`Initialized empty next-app project in: ${rootPath}`, false);
+    }
+
+  } catch (err: any) {
+    logger.error(err.message);
     process.exit(1);
   }
 }
-
