c548acbcf90f371b4e1e9cbdad944bbfd376957e
Lucas Araujo <contatodevaraujo@gmail.com>
2026-07-10 13:32:00 -0300

    refactor i18n: rename Index namespace to home to match conventions

--- src/templates/next-app/src/app/[locale]/page.tsx
+++ src/templates/next-app/src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from "next-intl";
 
 export default function HomePage() {
-  const t = useTranslations("Index");
+  const t = useTranslations("home");
   return (
     <div>
       <h1>{t("title")}</h1>
