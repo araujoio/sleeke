4e4467feb4315e3f1060b2a76c124775b0ff6193
Lucas Araujo <contatodevaraujo@gmail.com>
2026-07-31 21:14:59 -0300

    style(template): collapse layout function signature

--- src/templates/src/app/layout.tsx
+++ src/templates/src/app/layout.tsx
@@ -1,9 +1,7 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@wrksz/themes/next";
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
