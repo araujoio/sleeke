df5207c19d2898822b68fd9b955e4a3b646e0c96
Lucas Araujo <contatodevaraujo@gmail.com>
2026-08-01 21:15:14 -0300

    feat(template): add localized root layout with next-intl

---
+++ src/templates/src/app/[locale]/layout.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import { ThemeProvider } from "@wrksz/themes/next";
+import { notFound } from "next/navigation";
+import { getMessages, NextIntlClientProvider , hasLocale} from "next-intl";
+import { routing } from "@/i18n/routing";
+import { CookieUpdater } from "@/components/cookie-updater";
+
+export default async function RootLayout({ children, params }: Readonly<{ children: React.ReactNode; params: any }>) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html suppressHydrationWarning>
+      <body className="antialiased">
+        <CookieUpdater locale={locale} />
+        <NextIntlClientProvider messages={messages}>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
